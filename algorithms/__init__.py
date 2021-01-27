5178b9d9be40d7492a9d8d66981ac9c4b29d46d0
anonymous-researcher5 <77845801+anonymous-researcher5@users.noreply.github.com>
2021-01-27 15:33:02 +0100

    icml 2021

---
+++ algorithms/__init__.py
@@ -0,0 +1,4 @@
+# ICML 2021 State Representation Learning with Task-Irrelevant Factors of Variation in Robotics
+# Anonymous Authors 2021
+from .VAE_Algorithm import VAE_Algorithm
+from .AE_Algorithm import AE_Algorithm
